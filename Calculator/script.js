f3c9acce88a54cd481b5eef7ea8224d4440e4557
Daipayan Dey <daipayandey12@gmail.com>
2023-10-17 15:25:48 +0530

    This is a calculator

---
+++ Calculator/script.js
@@ -0,0 +1,50 @@
+let input = document.getElementById('input');
+let button = document.querySelectorAll('button');
+let string = "";
+Array.from(button).forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=')
+        {
+            if(string.includes('x')) {
+                string = string.replace('x', '*');
+                string = eval(string);
+                input.value = string;
+            }
+            else if(string.includes('÷')) {
+                string = string.replace('÷', '/');
+                string = eval(string);
+                input.value = string;
+            }
+            else if(string.includes('−')) {
+                string = string.replace('−', '-');
+                string = eval(string);
+                input.value = string;
+            }
+            else if(string.includes('+')) {
+                string = string.replace('+', '+');
+                string = eval(string);
+                input.value = string;
+            }
+            else if(string.includes('%')) {
+                string = string.replace('%', '%');
+                string = eval(string);
+                input.value = string;
+            }
+        }
+        else if (e.target.innerHTML == 'C')
+        {
+            string = "";
+            input.value = string;
+        }
+        else if (e.target.classList.contains('del-button'))
+        {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else
+        {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
+});
